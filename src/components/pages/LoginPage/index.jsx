db9e4c436b8a661c33384240f805e5c309207ce8
Doaa <doaa.id00@gmail.com>
2021-11-18 19:47:30 -0500

    adding shopping cart page and order component and login page

---
+++ src/components/pages/LoginPage/index.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+
+import './styles.css';
+
+export const LoginPage = () => {
+    const [mode, setMode] = useState("login");
+
+    const {register, handleSubmit} = useForm();
+
+    const loginUser = (formVals) => {
+        console.log("Login Submitted", formVals);
+    }
+
+    const signUpUser = (formVals) => {
+        console.log("Sign Up Submitted", formVals);
+    }
+
+    return (
+        <div className="pets-page">
+            
+            {mode === "login" && (
+                
+               <form className="form-layout" onSubmit={handleSubmit(loginUser)}>
+                   <h2>Welcome back, please sign in!</h2>
+                   <br />
+
+                   <label htmlFor="user">Username</label>
+                   <input type="email" name="user" required {...register('user')}/>
+
+                   <label htmlFor="password">Password</label>
+                   <input type="password" name="password" required {...register('password')}/>
+
+                   <input type="submit" value="login" />
+                   <br />
+                   <p>Don't have an account with us yet? Create a new account with your email and password.</p>
+                   <button onClick={() => setMode("signUp")}> Sing Up</button>
+               </form>     
+            )
+            }
+
+
+            {mode === "signup" && (
+                <form className="form-layout" onSubmit={handleSubmit(signUpUser)}>
+                    <h2>Creat a new account now!</h2>
+                    <br />
+
+                    <label htmlFor="user">Email</label>
+                    <input type="email" name="user" required {...register('user')}/>
+
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required {...register('password')} />
+
+                    <label htmlFor="passwordConfirm">Password</label>
+                    <input type="password" name="passwordConfirm" required {...register('passwordConfirm')}/>
+
+                    <input type="submit" value="login" />
+                   <br />
+                   <p>Have an account already?</p>
+                   <button onClick={() => setMode("login")}> Login</button>
+               </form>     
+            )
+            }       
+        </div>
+    )
+}
